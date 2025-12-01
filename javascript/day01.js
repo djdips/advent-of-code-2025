74b013fe1b3c6403eb7a60d18df0398e4ead00cc
Dipen Shah <dipen.shah@vayana.com>
2025-12-01 23:57:20 +0530

    Added Part 2 Solution for JS

--- javascript/day01.js
+++ javascript/day01.js
@@ -11,10 +11,12 @@ function part1(data) {
         const direction = instruction[0];
 
         if (direction === "L") {
-            currentPosition = (currentPosition - steps) % (maxPosition + 1);
+            currentPosition = currentPosition - steps;
         } else if (direction === "R") {
-            currentPosition = (currentPosition + steps) % (maxPosition + 1);
+            currentPosition = currentPosition + steps;
         }
+
+        currentPosition = currentPosition % (maxPosition + 1);
 
         console.log(`The dial is rotated ${direction}${steps} to point at ${currentPosition}.`);
 
@@ -32,7 +34,29 @@ function part1(data) {
 
 
 function part2(data) {
-    return null;
+    let currentPosition = 50;
+    let maxPosition = 99;
+    let countPositionZero = 0;
+
+    data.forEach((instruction) => {
+        const steps = parseInt(instruction.slice(1));
+        const direction = instruction[0];
+        const dialSize = maxPosition + 1;
+
+        for (let i = 0; i < steps; i++) {
+            if (direction === "L") {
+                currentPosition = (currentPosition - 1 + dialSize) % dialSize;
+            } else {
+                currentPosition = (currentPosition + 1) % dialSize;
+            }
+
+            if (currentPosition === 0) countPositionZero++;
+        }
+
+        console.log(`The dial is rotated ${direction}${steps} to point at ${currentPosition}.`);
+    });
+    
+    return countPositionZero;
 }
 
 
